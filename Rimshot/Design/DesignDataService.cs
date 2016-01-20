1932c0ac1f5d8d1be44dd4ce665168750faf48d9
dupuyjs <jdupuy@microsoft.com>
2016-01-20 19:59:15 +0100

    Initial commit

---
+++ Rimshot/Design/DesignDataService.cs
@@ -0,0 +1,17 @@
+﻿using Rimshot.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rimshot.Design
+{
+    public class DesignDataService : IDataService
+    {
+        public Task<string> GetData()
+        {
+            return Task.FromResult("design");
+        }
+    }
+}
---
+++ Rimshot/Model/DataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rimshot.Model
+{
+    class DataService : IDataService
+    {
+        public Task<string> GetData()
+        {
+            return Task.FromResult("item");
+        }
+    }
+}
